1fa00b709498d93b836274d3ef8c003ea0a64810
aleksandrdremov <dremov.me@gmail.com>
2021-03-17 14:41:03 +0300

    Shell scripts, garbage

--- generateTest.sh
+++ generateTest.sh
@@ -1,3 +1,5 @@
+rm -rf data
+mkdir -p data
 cd data || return 1;
 
 for i in 1 2 3 4 5 6 7 8
---
+++ sort.sh
@@ -0,0 +1,1 @@
+build/coSort 880 data/1.txt data/2.txt data/3.txt data/4.txt data/5.txt data/6.txt data/7.txt data/8.txt
